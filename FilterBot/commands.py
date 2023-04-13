7bedfca590a0c01b2a6305ae8b9f6b17100a3dc7
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-04-13 18:24:45 +0530

    Update commands.py

--- FilterBot/commands.py
+++ FilterBot/commands.py
@@ -48,7 +48,7 @@ async def aboutCMD(client: FilterBot, message: Message):
         await db.add_user(message.from_user.first_name, message.from_user.id)
 
     keyboard = [[ InlineKeyboardButton('Tutorial', url='t.me/mo_tech_yt'),
-                   InlineKeyboardButton('Repo', url='https://github.com/PR0FESS0R-99/Filter-Bot') ],
+                   InlineKeyboardButton('Repo', url='https://github.com/PR0FESS0R-99/FilterBot') ],
                 [ InlineKeyboardButton('Home', callback_data='main#start'),
                   InlineKeyboardButton('Help', callback_data='main#help') ]]
 
@@ -83,7 +83,7 @@ async def maincallback(client: FilterBot, message):
 
     elif type == "about":
         keyboard = [[ InlineKeyboardButton('Tutorial', url='t.me/mo_tech_yt'),
-                       InlineKeyboardButton('Repo', url='https://github.com/PR0FESS0R-99/Filter-Bot') ],
+                       InlineKeyboardButton('Repo', url='https://github.com/PR0FESS0R-99/FilterBot') ],
                     [ InlineKeyboardButton('Home', callback_data='main#start'),
                       InlineKeyboardButton('Help', callback_data='main#help') ]]
         await message.message.edit(text=AboutTxT, reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True)
