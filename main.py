61ee2d78fe91e0240722079c70df074f86ea7a10
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-04-18 14:02:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from aiohttp import web
+from route import web_server
 from pyrogram import Client, __version__
-from configs import API_ID, API_HASH, BOT_TOKEN, LOGGER
+from configs import API_ID, API_HASH, BOT_TOKEN, LOGGER, PORT
 
 class FilterBot(Client):
 
@@ -13,6 +15,10 @@ class FilterBot(Client):
         await super().start()
         me = await self.get_me()
         self.LOGGER(__name__).info(f"{me.first_name} with for Pyrogram v{__version__} started on {me.username}.")
+
+        app = web.AppRunner(await web_server())
+        await app.setup()       
+        await web.TCPSite(app, "0.0.0.0", PORT).start()
 
     async def stop(self, *args):
         await super().stop()
